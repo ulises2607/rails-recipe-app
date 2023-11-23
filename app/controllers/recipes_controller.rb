4643a1c9f26da97b09bb7507d8b88e3e2df5d739
Ulises <ulisescappa@gmail.com>
2023-11-23 17:01:47 -0300

    Add new recipe view. Add show recipe views with a section for a recipe details.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,12 @@
 class RecipesController < ApplicationController
+    before_action :find_recipe, only: %i[show destroy update]
+    before_action :authenticate_user!, except: %i[index show]
+
   def index
     @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find_by_id(params[:id])
   end
 
   def new
@@ -12,18 +14,39 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.create(recipe_params)
-    redirect_to recipe_path(@recipe)
+        @recipe = Recipe.new(recipe_params)
+        @recipe.user = current_user
+        puts params.inspect
+        if @recipe.save
+            redirect_to recipe_path(@recipe)
+        else
+            render :new
+        end
+    
   end
 
   def destroy
-    @recipe = Recipe.find_by_id(params[:id])
     @recipe.destroy
     puts 'Receta eliminada correctamente' if @recipe.destroyed?
-    redirect_to recipes_path(@recipe)
+    redirect_to recipes_path
+  end
+
+  def update
+    @recipe.toggle!(:public) # Utiliza toggle! para cambiar el valor de public
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
+
+  def find_recipe
+    @recipe = Recipe.find_by_id(params[:id])
+    end
 
   def recipe_params
     params.require(:recipe).permit(
