858566b057f3bc358b4cab9f3397ac53808982ee
SFarida <faridagueleu@gmail.com>
2023-11-23 19:35:47 +0100

    Add navigation

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  resources :foods
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+  resources :foods, path: 'foods'
+  get '/general_shopping_list', to: 'general_shopping_list#index'
 end
