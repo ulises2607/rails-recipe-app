46b15f35f15b72709a039ccbe27773a279daac89
SFarida <faridagueleu@gmail.com>
2023-11-24 23:25:21 +0100

    Add total foods and price for public recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,16 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_food, dependent: :destroy
+  has_many :food, through: :recipe_food
 
   validates :name, presence: true
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
   validates :description, presence: true
+
+  def count_recipe_foods
+    count = recipe_food.count
+    sum_price = food.sum(:price)
+    { count: count, sum_price: sum_price }
+  end
 end
