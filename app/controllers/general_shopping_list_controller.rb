0416660521ae5895595955b1f88cb3b8012f2f10
SFarida <faridagueleu@gmail.com>
2023-11-24 09:05:54 +0100

    Fix linters

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -5,10 +5,10 @@ class GeneralShoppingListController < ApplicationController
     @recipe_foods = []
     @recipes.each do |recipe|
       recipe.recipe_food.each do |recipe_food|
-          @recipe_foods << recipe_food
+        @recipe_foods << recipe_food
       end
     end
     foods_in_recipes_ids = @recipe_foods.map(&:food_id).uniq
-    @missing_foods = @foods.where.not(id: foods_in_recipes_ids)    
+    @missing_foods = @foods.where.not(id: foods_in_recipes_ids)
   end
 end
