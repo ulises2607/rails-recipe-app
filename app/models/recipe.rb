80aeab0a85cb7508ab6613d242101adc05be538f
Ulises <ulisescappa@gmail.com>
2023-11-24 20:34:21 -0300

    Add validations to the recipe model. Add factory bot for recipe.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,10 @@ class Recipe < ApplicationRecord
   has_many :recipe_food, dependent: :destroy
 
   validates :name, presence: true
-  validates :preparation_time, presence: true
-  validates :cooking_time, presence: true
+  validates :preparation_time, presence: true, numericality: { only_integer: true }
+  validates :cooking_time, presence: true, numericality: { only_integer: true }
   validates :description, presence: true
+  validates :public, presence: true, inclusion: { in: [true, false] }
+  validates :user_id, presence: true
+
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryBot.create(:recipe) }
+
+  before { subject.save }
+
+  it 'should have a name' do
+    subject.name = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'should have a valid preparation time' do
+    subject.preparation_time = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'should have a valid cooking time' do
+    subject.cooking_time = nil
+    expect(subject).not_to be_valid
+  end
 end
