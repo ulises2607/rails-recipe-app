48fa5665e163f6c80ffd2a773caa0e80413b8df9
Ulises <ulisescappa@gmail.com>
2023-11-24 22:00:59 -0300

    Add recipes and foods requests tests.

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,7 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe 'Foods', type: :request do
+RSpec.describe 'FoodsController', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      user = FactoryBot.create(:user)
+      post '/users/sign_in', params: { user: { email: user.email, password: user.password } }
+      get foods_path
+    end
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'correct template is rendered' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'the response body includes correct placeholder text' do
+      expect(response.body).to include('Food')
+    end
+  end
+
+  describe 'GET /show' do
+    before do
+      user = FactoryBot.create(:user)
+      post '/users/sign_in', params: { user: { email: user.email, password: user.password } }
+      food = FactoryBot.create(:food)
+      get food_path(food)
+    end
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'correct template is rendered' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'the response body includes correct placeholder text' do
+      food = FactoryBot.create(:food)
+      get food_path(food)
+      expect(response.body).to include('Welcome')
+    end
   end
 end
